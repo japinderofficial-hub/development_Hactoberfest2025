48fc6e4af6a2bd5950828e4f9b5d0595fd5f4589
japinderofficial-hub <japinderofficial@gmail.com>
2025-10-27 16:25:11 +0530

    Create JapinderKaur_Hacktoberfest2025.py
    Create JapinderKaur_Hacktoberfest2025.py
    my first pull request

---
+++ JapinderKaur_Hacktoberfest2025.py
@@ -0,0 +1,17 @@
+def welcome_message():
+    print("🌸 Hello, Open Source World! 🌸")
+    print("I'm Japinder Kaur, and this is my first contribution for Hacktoberfest 2025!")
+    print("Excited to learn, grow, and collaborate with amazing developers! 🚀✨")
+    print("Let's make coding fun and meaningful together ❤️")
+
+def hacktoberfest_spirit():
+    print("\n🔥 Hacktoberfest Spirit 🔥")
+    print("-> Code 💻")
+    print("-> Contribute 🤝")
+    print("-> Celebrate 🎉")
+    print("-> Repeat ♻️")
+    print("\nHappy Hacking and Happy Learning! 💪💻")
+
+if __name__ == "__main__":
+    welcome_message()
+    hacktoberfest_spirit()
